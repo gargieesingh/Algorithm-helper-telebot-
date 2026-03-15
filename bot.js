b762b75bbb49ede4dcda6911b6ccbbca08713015
Gargie <gargiesingh321@gmail.com>
2026-03-15 10:07:40 +0530

    algorithm logic

--- bot.js
+++ bot.js
@@ -1,3 +1,36 @@
 const {Telegraf} = require('telegraf');
+require('dotenv').config();
 
-const bot = new Telegraf('8724276456:AAFtIKUM3q7a1sbqEphR_z8WunFt4drHc0g');
+const bot = new Telegraf('process.env.BOT_TOKEN');
+
+const algorithms = {
+    "binary search" : `
+    Binary Seach (cpp):
+    
+    int binarySearch(int arr[], int l, int r, int x){
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) return mid;
+            if (arr[mid] > x) return binarySearch(arr, l, mid - 1, x);
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+        
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+    `,
+    "linear search": `
+    Linear Search (cpp):
+    
+    int linearSearch(int arr[], int n, int x) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) return i;
+        }
+            return -1;
+    }
+    
+    Time Complexity: O(n)
+    Space Complexity: O(1)`
+}
+
